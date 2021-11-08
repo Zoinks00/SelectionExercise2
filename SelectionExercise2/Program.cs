a60485badd05bd8cf9b24055a9927d0984526405
Forrest Rabideau <forrest.rabideau@gmail.com>
2021-11-08 11:34:34 -0800

    1st commit

---
+++ SelectionExercise2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SelectionExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputer = " ";
+            
+
+            Console.WriteLine("Please enter a school subject and press the enter key.");
+
+            userInputer = Console.ReadLine();
+           
+        switch (userInputer)
+
+            {//start of switch
+
+                case "science":
+                    Console.WriteLine("You like making things go boom dont you.");
+                    break;
+                case "math":
+                    Console.WriteLine("You must be a nerd.");
+                    break;
+                case "english":
+                    Console.WriteLine("I are gud at grammar too.");
+                    break;
+                case "art":
+                    Console.WriteLine("A very relexing subject.");
+                    break;
+                case "pe":
+                    Console.WriteLine("You must watch a lot of sports.");
+                    break;
+                default:
+                    Console.WriteLine("You have not entered a selectable option. Please try again using");
+                    Console.Write("science, math, english, art or pe as selectable option.");
+                    break;
+            }// end of switch
+            
+        }
+    }
+}
